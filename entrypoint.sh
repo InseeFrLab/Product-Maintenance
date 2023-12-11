9b38138f23a3acf0fd63cc2a476a93b6d0afbada
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-12-11 11:33:01 +0100

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "window._env_['VITE_PRODUCT'] = `$VITE_PRODUCT`;" >> /usr/share/nginx/html/env-config.js
-echo "window._env_['VITE_EMAIL_BUSINESS'] = `$VITE_EMAIL_BUSINESS`;" >> /usr/share/nginx/html/env-config.js
-echo "window._env_['VITE_EMAIL_INFO'] = `$VITE_EMAIL_INFO`;" >> /usr/share/nginx/html/env-config.js
-echo "window._env_['VITE_MAINTENANCE_REASON'] = `$VITE_MAINTENANCE_REASON`;" >> /usr/share/nginx/html/env-config.js
+echo "window._env_['VITE_PRODUCT'] = \"$VITE_PRODUCT\";" >> /usr/share/nginx/html/env-config.js
+echo "window._env_['VITE_EMAIL_BUSINESS'] = \"$VITE_EMAIL_BUSINESS\";" >> /usr/share/nginx/html/env-config.js
+echo "window._env_['VITE_EMAIL_INFO'] = \"$VITE_EMAIL_INFO\";" >> /usr/share/nginx/html/env-config.js
+echo "window._env_['VITE_MAINTENANCE_REASON'] = \"$VITE_MAINTENANCE_REASON\";" >> /usr/share/nginx/html/env-config.js
 exec "$@"
